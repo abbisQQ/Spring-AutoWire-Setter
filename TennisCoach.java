45548f8dbfed52416451425c5a56013b99f0c058
Ch. Theodorou <theodoroucharalabos@gmail.com>
2018-01-18 23:45:36 +0200

    Create TennisCoach.java

---
+++ TennisCoach.java
@@ -0,0 +1,41 @@
+package com.luv2code.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+//bean id someSillyCoach
+@Component("someSillyCoach")
+public class TennisCoach implements Coach {
+
+	
+	private FortuneService FortuneService;
+	
+	public TennisCoach() {
+		System.out.println("Inside tennis coach instructor");
+	}
+	
+
+
+
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("inside setter method");
+		FortuneService = fortuneService;
+	}
+
+
+
+
+	@Override
+	public String getDailyWorkOut() {
+		// TODO Auto-generated method stub
+		return "Back hand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return FortuneService.getFortune();
+	}
+
+}
